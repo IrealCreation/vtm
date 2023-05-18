af10a414dcc62ba02081ae7a6183e9da188cee08
Srikahn <49712202+IrealCreation@users.noreply.github.com>
2023-05-18 12:46:45 +0200

    Replacing with html entities

--- pages/lignees.tsx
+++ pages/lignees.tsx
@@ -24,9 +24,9 @@ export default function PageLignees() {
                     <h1>Lignées</h1>
                     <div className="title-line"></div>
                 </div>
-                <p>Les vampires, aussi nommés <em>caïnites</em> à cause du mythe qui en fait des descendants de Caïn après que celui-ci ait été maudit par Dieu pour avoir tué son frère, se répartissent en plusieurs "lignées" ou "clans".</p>
-                <p>Lorsqu'un vampire décide d'en engendrer un autre lors d'un processus nommé <em>l'Étreinte</em>, il s'abreuve du sang de sa victime jusqu'à la laisser exsangue puis lui donne à boire son propre flux vital. Le nouveau-né, désigné sous le terme <em>infant</em>, hérite de la lignée de son géniteur, désigné sous le terme de <em>sire</em>.</p>
-                <p>Chaque lignée dispose de ses propres spécificités et de ses propres <Link href="/disciplines">disciplines</Link> (capacités surnaturelles à sa disposition). Elles ont également leurs propres traditions internes et leurs propres habitudes transmises de sire à infant. Cependant, le caractère d'un caïnite ne se définit pas uniquement par sa lignée, et il faut se garder de tout stéréotype hâtif.</p>
+                <p>Les vampires, aussi nommés <em>caïnites</em> à cause du mythe qui en fait des descendants de Caïn après que celui-ci ait été maudit par Dieu pour avoir tué son frère, se répartissent en plusieurs &quot;lignées&quot; ou &quot;clans&quot;.</p>
+                <p>Lorsqu&apos;un vampire décide d&apos;en engendrer un autre lors d&apos;un processus nommé <em>l&apos;Étreinte</em>, il s&apos;abreuve du sang de sa victime jusqu&apos;à la laisser exsangue puis lui donne à boire son propre flux vital. Le nouveau-né, désigné sous le terme <em>infant</em>, hérite de la lignée de son géniteur, désigné sous le terme de <em>sire</em>.</p>
+                <p>Chaque lignée dispose de ses propres spécificités et de ses propres <Link href="/disciplines">disciplines</Link> (capacités surnaturelles à sa disposition). Elles ont également leurs propres traditions internes et leurs propres habitudes transmises de sire à infant. Cependant, le caractère d&apos;un caïnite ne se définit pas uniquement par sa lignée, et il faut se garder de tout stéréotype hâtif.</p>
                 {jsxLignees}
             </section>
         </>
