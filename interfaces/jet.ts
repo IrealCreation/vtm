6df525ae8154266758c596a3d6489c6e4a7bd822
Srikahn <49712202+IrealCreation@users.noreply.github.com>
2023-06-06 16:49:29 +0200

    Preparing the dices

---
+++ interfaces/jet.ts
@@ -0,0 +1,13 @@
+export interface Jet {
+    id: number,
+    stat1: string,
+    value1: number,
+    stat2: string,
+    value2: number,
+    bonus: number,
+    success: number,
+    soif: number,
+    eveil: number,
+    json: string,
+    perso_id: number
+}
