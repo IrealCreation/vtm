3339be401ac64e3bd3416ebc4fe3a6b237d575b6
Srikahn <49712202+IrealCreation@users.noreply.github.com>
2023-06-12 12:10:08 +0200

    Index connecté / déconnecté

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,26 @@
-import Head from "next/head";
 import Link from "next/link";
 import Navbar from "@/components/navbar";
+import { useIsLogged } from "@/hooks/useIsLogged";
 
 export default function Home() {
+
+    const [isLogged, setIsLogged] = useIsLogged();
+
+    const jsxLiens: Array<JSX.Element> = [];
+
+    if(isLogged) {
+        jsxLiens.push(<Link key="fiche" href="/fiche" className="btn-lg">Fiche personnage</Link>);
+        jsxLiens.push(<Link key="des" href="/des" className="btn-lg">Plateau des dés</Link>);
+        jsxLiens.push(<Link key="documentation" href="/documentation" className="btn-lg">Documentation</Link>);
+        jsxLiens.push(<Link key="profil" href="/profil" className="btn-lg">Profil</Link>);
+    }
+    else {
+        jsxLiens.push(<Link key="fiche" href="/fiche" className="btn-lg">Fiche personnage (exemple)</Link>);
+        jsxLiens.push(<Link key="documentation" href="/documentation" className="btn-lg">Documentation</Link>);
+        jsxLiens.push(<Link key="connexion" href="/connexion" className="btn-lg">Connexion</Link>);
+        jsxLiens.push(<Link key="inscription" href="/inscription" className="btn-lg">Inscription</Link>);
+    }
+
     return(
         <>
             <section className="index main-container">
@@ -11,10 +29,7 @@ export default function Home() {
                   <h1>JdR <br/>Vampire the Masquerade</h1>
                   <div className="title-line"></div>
               </div>
-              <Link href="/fiche" className="btn-lg">Fiche personnage</Link>
-              <Link href="/lignees" className="btn-lg">Lignées</Link>
-              <Link href="/disciplines" className="btn-lg">Disciplines</Link>
-              <Link href="/regles" className="btn-lg">Règles de jeu</Link>
+              {jsxLiens}
             </section>
             <Navbar/>
         </>
